80b04deb417d3074b06da8ecdaf1a94d68844388
Alex <alex.minati1993@gmail.com>
2024-02-25 15:32:04 +0100

    POST and GET thread done, reconstructed DB more changes will occur later on

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,111 @@
-'use strict';
+const sqlite3 = require("sqlite3").verbose();
 
+// Check out:
+// https://www.sqlitetutorial.net/sqlite-nodejs/
 
+// Opening database with read and writes permission.
+let db = new sqlite3.Database(
+  "./messageboard.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+      return res
+        .status(500)
+        .json({ error: "Error connecting to the database" });
+    }
+    console.log("Connected to DB");
+  }
+);
+
+// Closing database connection when the server is closed
+process.on("SIGINT", function () {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Closed DB");
+    process.exit(0); // Exit the process after closing the database
+  });
+});
 
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  // POST method. Post text and delete_password and add to db. Board is gathered by the board params.
+  app.route("/api/threads/:board").post((req, res) => {
+    // For the board name we are posting on.
+    const board = req.params.board;
+    const { text, delete_password } = req.body;
 
+    // Insert new thread into the database
+    let sql = `INSERT INTO thread (board, text, delete_password) VALUES (?, ?, ?)`;
+    db.run(sql, [board, text, delete_password], function (err) {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      res.redirect("back");
+    });
+  });
+
+  // GET method. Get the recent 10 threads on the board and with only 3 replies for each thread.
+  app.route("/api/threads/:board").get((req, res) => {
+    const board = req.params.board;
+
+    // Want only to retrieve the 10 recent bumped threads on the board.
+    // For board we check with "WHERE board = ?" And input this as sql search.
+    let selectThreadsSql = `
+            SELECT * FROM thread 
+            WHERE board = ? 
+            ORDER BY bumped_on DESC 
+            LIMIT 10`;
+    db.all(selectThreadsSql, [board], (err, threads) => {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      // Array with the threads and included the 3 replies.
+      const ThreadsWithReplies = [];
+      let number_of_threads = 0;
+
+      // Iterating through threads
+      threads.forEach((thread) => {
+        // Select query we want to see the 3 most recent replies for each thread.
+        let selectRepliesSql = `
+                    SELECT * FROM replies 
+                    WHERE thread_id = ? 
+                    ORDER BY created_on DESC 
+                    LIMIT 3`;
+        db.all(selectRepliesSql, [thread._id], (err, replies) => {
+          if (err) {
+            console.error(err.message);
+            return res.status(500).json({ error: "Internal Server Error" });
+          }
+
+          // Deleting delete_password and reported fields from all the threads
+          delete thread.delete_password;
+          delete thread.reported;
+
+          // Deleting delete_password and reported fields from the 3 recent replies
+          replies.forEach((reply) => {
+            delete reply.delete_password;
+            delete reply.reported;
+          });
+
+          // Adding the replies to the thread object and incrementing the thread count
+          thread.replies = replies;
+          // Appending the thread to the array
+          ThreadsWithReplies.push(thread);
+          // Increment the number of threads
+          number_of_threads++;
+          // console.log(number_of_threads, threads.length);
+          // Return response when we reached the actual count of threads in the board. Max:10
+          if (number_of_threads === threads.length)
+            res.json(ThreadsWithReplies);
+        });
+      });
+    });
+  });
+  app.route("/api/replies/:board");
 };
--- server.js
+++ server.js
@@ -1,105 +1,67 @@
-'use strict';
-require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-const helmet = require("helmet"); 
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const bcrypt = require("bcrypt");
 
-const sqlite3 = require("sqlite3").verbose(); 
-
-// Opening database. 
-const db = new sqlite3.Database("./messageboard.db", sqlite3.OPEN_READWRITE, (err)=>{
-  if (err)
-    return console.error(err.message); 
-
-  console.log("Connected to DB"); 
-});
-
-// Insertion to db test
-// const sql = `INSERT INTO threads (_id, text, delete_password, replies) VALUES (?, ?, ?, ?)`;
-// db.run(sql, ["1","hello world", "1234", "hdajsdhsajda" ], (err)=>{
-//   if (err)
-//   return console.error(err.message); 
-//   console.log("Success insertion to DB"); 
-// }); 
-
-
-
-
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-// Middlewares 
+// Middlewares
 app.use(helmet.frameguard());
 app.use(helmet.dnsPrefetchControl()); // Disable DNS fetching
-app.use(helmet.referrerPolicy({policy: "same-origin"}));  // Only referrer on own pages
+app.use(helmet.referrerPolicy({ policy: "same-origin" })); // Only referrer on own pages
 
-
-
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
+app.route("/b/:board/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/board.html");
+});
+app.route("/b/:board/:threadid").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/thread.html");
+});
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
+//Routing for API
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  console.log("Your app is listening on port " + listener.address().port);
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
-        console.log('Tests are not valid:');
+      } catch (e) {
+        console.log("Tests are not valid:");
         console.error(e);
       }
     }, 1500);
   }
-});
-
-
-// Closing database connection when the server is closed. 
-process.on("SIGINT", function () {
-  db.close((err) => {
-    if (err)
-      return console.error(err.message);
-    console.log("Closed DB");
-    process.exit(0); // Exit the process after closing the database
-  });
 });
 
 module.exports = app; //for testing
